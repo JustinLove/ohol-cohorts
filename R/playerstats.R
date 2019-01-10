3ac4f4364b6d88cdfb23dc08ef676fc8411fd452
Justin Love <git@JustinLove.name>
2019-01-10 11:06:10 -0600

    use week based year for proper cohort allocation

--- R/playerstats.R
+++ R/playerstats.R
@@ -8,8 +8,8 @@ playerstats <- function(lives) {
   print("calculate started")
   lives$started <- ave(lives$birthtime, lives$hash, FUN=min)
   # needs to be fixed for different years
-  lives$startweek <- strftime(as.POSIXct(lives$started, tz = "GMT", "1970-01-01"), format="%Y-%V")
-  lives$birthweek <- strftime(as.POSIXct(lives$birthtime, tz = "GMT", "1970-01-01"), format="%Y-%V")
+  lives$startweek <- strftime(as.POSIXct(lives$started, tz = "GMT", "1970-01-01"), format="%G-%V")
+  lives$birthweek <- strftime(as.POSIXct(lives$birthtime, tz = "GMT", "1970-01-01"), format="%G-%V")
   lives$daysowned <- round((lives$birthtime - lives$started) / (60*60*24))
   lives$gameday <- round((lives$birthtime - lives$birthtime[1]) / (60*60*24))
   lives$lt1 <- lives$lifetime < 1
